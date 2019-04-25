1d6f099c668bf5c1b479d0dfd10dd4338b83ef07
Ewan McAndrew <ewan.m@hotmail.com>
2019-04-25 12:52:05 +1000

    Convert tabs

--- cin.cpp
+++ cin.cpp
@@ -23,32 +23,32 @@ typedef std::vector<std::vector<std::string>> Board;
 
 // Print the board
 void display(Board board) {
-	std::stringstream ss;
-	for (auto & v : board) {
-		ss << "\t";
-		for (auto & s : v) {
-			ss << s << " ";
-		}
-		ss << endl;
-	}
-	ss << endl << endl;
-	cout << ss.str() ;
+    std::stringstream ss;
+    for (auto & v : board) {
+        ss << "\t";
+        for (auto & s : v) {
+            ss << s << " ";
+        }
+        ss << endl;
+    }
+    ss << endl << endl;
+    cout << ss.str() ;
 }
 
 // Find the starting point (start at 0,0 if not specified).
 Node find_start(Board board) {
-	for (int i = 0; i < board.size(); ++i) {
-		for (int j = 0; j < board[i].size(); ++j) {
-			if (IS_START(board[i][j])) 
-				return { i, j };
-		}
-	}
-	return { 0, 0 };
+    for (int i = 0; i < board.size(); ++i) {
+        for (int j = 0; j < board[i].size(); ++j) {
+            if (IS_START(board[i][j])) 
+                return { i, j };
+        }
+    }
+    return { 0, 0 };
 }
 
 // Add the children of the front node to the end of the queue
 void add_children(int size, std::queue<Node>& q) {
-	Node n(q.front());
+    Node n(q.front());
 
     if (n.first > 0) {
         q.push({n.first-1, n.second});
@@ -66,30 +66,30 @@ void add_children(int size, std::queue<Node>& q) {
 
 // Breadth first search (iterative)
 int bfs(Board board) {
-	std::chrono::milliseconds t(150);
+    std::chrono::milliseconds t(150);
 
     int steps(0);
     std::queue<Node> q;
     q.push(find_start(board));
     q.push({-1, -1});
-	add_children(board.size(), q);
+    add_children(board.size(), q);
 
     while(!q.empty()) {
         if (q.front().first == -1) {
             ++steps;
-			q.push({ -1, -1 });
-		} else if (board[q.front().first][q.front().second] == "*") {
-			q.pop();
-			continue;
+            q.push({ -1, -1 });
+        } else if (board[q.front().first][q.front().second] == "*") {
+            q.pop();
+            continue;
         } else if (IS_END(board[q.front().first][q.front().second])) {
             return steps;
         } else if (!IS_WALL(board[q.front().first][q.front().second]) && 
-				   !IS_START(board[q.front().first][q.front().second])) {
+                   !IS_START(board[q.front().first][q.front().second])) {
             add_children(board.size(), q);
-			board[q.front().first][q.front().second] = "*";
-			display(board);
-			std::this_thread::sleep_for(t);
-		}
+            board[q.front().first][q.front().second] = "*";
+            display(board);
+            std::this_thread::sleep_for(t);
+        }
         q.pop();
     }
 
@@ -121,7 +121,7 @@ std::vector<std::string> split_string(std::string input_string) {
 
 int main(int argc, char* argv[]) 
 {
-	// Load the board
+    // Load the board
     Board origBoard;
     std::string line;
     std::ifstream fs;
@@ -129,18 +129,18 @@ int main(int argc, char* argv[])
     if (fs.is_open()) {
         while (std::getline(fs, line)) {
             cout << line << endl;
-			origBoard.push_back(split_string(line));
+            origBoard.push_back(split_string(line));
         }
         fs.close();
     } else {
         cout << "failed to open file: " << argv[1] << endl;
     }
 
-	// Run the game
-	Board board(origBoard);
-	do {
-		cout << "Minimum steps: " << bfs(board) << endl;
-		cout << "[q] to quit or [r] to run again" << endl;
-		cin >> line;
-	} while (line == "r");
+    // Run the game
+    Board board(origBoard);
+    do {
+        cout << "Minimum steps: " << bfs(board) << endl;
+        cout << "[q] to quit or [r] to run again" << endl;
+        cin >> line;
+    } while (line == "r");
 }
